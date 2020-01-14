277e38a20013d172756e537f52ddac652b477d26
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2020-01-14 17:16:31 +0000

    Added copyright header and file.

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# (c) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT
+# which you should have received as part of this distribution
+
 import os.path
 import setuptools
 
--- source/fab/__init__.py
+++ source/fab/__init__.py
@@ -1,3 +1,7 @@
+# (c) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT
+# which you should have received as part of this distribution
+
 '''
 Flexible build system for scientific software.
 '''
