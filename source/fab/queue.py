17d02161a9ef642ce913d9291b91bdf6a6cee5e4
Steve Wardle <steven.wardle@metoffice.gov.uk>
2020-01-31 16:09:57 +0000

    Actually add the files this time

---
+++ source/fab/queue.py
@@ -0,0 +1,12 @@
+##############################################################################
+# (c) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT
+# which you should have received as part of this distribution
+##############################################################################
+'''
+Classes and methods relating to main transformation
+'''
+
+class QueueManager(object):
+    def __init__(self):
+        pass
