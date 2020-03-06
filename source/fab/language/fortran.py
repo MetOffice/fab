4749df1739e671fcd1bf3216238fbb7f1a2d38cf
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2020-03-06 15:44:17 +0000

    Convert the Fortran source normalising function into a TextReader decorator.

--- source/fab/language/fortran.py
+++ source/fab/language/fortran.py
@@ -9,6 +9,7 @@ import logging
 from pathlib import Path
 import re
 from typing import (Generator,
+                    Iterator,
                     List,
                     Match,
                     Optional,
@@ -23,7 +24,7 @@ from fab.database import (DatabaseDecorator,
                           SqliteStateDatabase,
                           WorkingStateException)
 from fab.language import Analyser, AnalysisException
-from fab.reader import TextReader
+from fab.reader import TextReader, TextReaderDecorator
 
 
 class FortranWorkingState(DatabaseDecorator):
@@ -196,6 +197,44 @@ class FortranWorkingState(DatabaseDecorator):
         return units
 
 
+class _FortranNormaliser(TextReaderDecorator):
+    def __init__(self, source: TextReader):
+        super().__init__(source)
+        self._line_buffer = ''
+
+    def line_by_line(self) -> Iterator[str]:
+        '''
+        Each line of the source file is modified to ease the work of analysis.
+
+        The lines are sanitised to remove comments and collapse the result
+        of continuation lines whilst also trimming away as much whitespace as
+        possible
+        '''
+        for line in self._source.line_by_line():
+            # Remove comments - we accept that an exclamation mark
+            # appearing in a string will cause the rest of that line
+            # to be blanked out, but the things we wish to parse
+            # later shouldn't appear after a string on a line anyway
+            line = re.sub(r'!.*', '', line)
+
+            # If the line is empty, go onto the next
+            if line.strip() == '':
+                continue
+
+            # Deal with continuations by removing them to collapse
+            # the lines together
+            self._line_buffer += line
+            if "&" in self._line_buffer:
+                self._line_buffer = re.sub(r'&\s*\n', '', self._line_buffer)
+                continue
+
+            # Before output, minimise whitespace but add a space on the end
+            # of the line.
+            line_buffer = re.sub(r'\s+', r' ', self._line_buffer)
+            yield line_buffer.rstrip()
+            self._line_buffer = ''
+
+
 class FortranAnalyser(Analyser):
     def __init__(self, database: SqliteStateDatabase):
         super().__init__(database)
@@ -258,8 +297,9 @@ class FortranAnalyser(Analyser):
 
         self._state.remove_fortran_file(source.get_filename())
 
+        normalised_source = _FortranNormaliser(source)
         scope: List[Tuple[str, str]] = []
-        for line in self._normalise(source):
+        for line in normalised_source.line_by_line():
             logger.debug(scope)
             logger.debug('Considering: %s', line)
 
@@ -355,36 +395,3 @@ class FortranAnalyser(Analyser):
                                       'found': end_name}
                         raise AnalysisException(
                             end_message.format(**end_values))
-
-    @staticmethod
-    def _normalise(source: TextReader) -> Generator[str, None, None]:
-        '''
-        Generator to return each line of a source file; the lines
-        are sanitised to remove comments and collapse the result
-        of continuation lines whilst also trimming away as much
-        whitespace as possible
-        '''
-        line_buffer = ''
-        for line in source.line_by_line():
-            # Remove comments - we accept that an exclamation mark
-            # appearing in a string will cause the rest of that line
-            # to be blanked out, but the things we wish to parse
-            # later shouldn't appear after a string on a line anyway
-            line = re.sub(r'!.*', '', line)
-
-            # If the line is empty, go onto the next
-            if line.strip() == '':
-                continue
-
-            # Deal with continuations by removing them to collapse
-            # the lines together
-            line_buffer += line
-            if "&" in line_buffer:
-                line_buffer = re.sub(r'&\s*\n', '', line_buffer)
-                continue
-
-            # Before output, minimise whitespace but add a space on the end
-            # of the line.
-            line_buffer = re.sub(r'\s+', r' ', line_buffer)
-            yield line_buffer.rstrip()
-            line_buffer = ''
