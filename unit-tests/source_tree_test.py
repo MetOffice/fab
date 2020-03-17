59d01db468e604c57ac21cb577412c822c235ea5
Steve Wardle <steven.wardle@metoffice.gov.uk>
2020-03-17 15:40:49 +0000

    Expanded source tree testing to cover `Command` case, and nesting

--- unit-tests/source_tree_test.py
+++ unit-tests/source_tree_test.py
@@ -4,10 +4,10 @@
 # which you should have received as part of this distribution
 ##############################################################################
 from pathlib import Path
-from typing import Iterator, Union, List, Mapping
+from typing import Iterator, Union, List, Mapping, Dict, Union, Type
 
 from fab.database import SqliteStateDatabase, FileInfoDatabase, FileInfo
-from fab.language import Analyser
+from fab.language import Analyser, Command, Task
 from fab.reader import TextReader
 from fab.source_tree import ExtensionVisitor, TreeDescent, TreeVisitor
 
@@ -47,21 +47,33 @@ class DummyReader(TextReader):
 
 
 tracker: Mapping[str, List[Path]] = {
-    "FOO": [],
-    "BAR": [],
+    "analyser": [],
+    "command": [],
 }
 
 
-class DummyAnalyserFoo(Analyser):
+def clear_tracker():
+    tracker["analyser"] = []
+    tracker["command"] = []
+
+
+class DummyAnalyser(Analyser):
     def run(self):
-        tracker["FOO"].append(self._reader)
+        tracker["analyser"].append(self._reader)
         return []
 
 
-class DummyAnalyserBar(Analyser):
-    def run(self):
-        tracker["BAR"].append(self._reader)
-        return []
+class DummyCommand(Command):
+    @property
+    def as_list(self) -> List[str]:
+        tracker["command"].append(self._filename)
+        # Note that this is the command "true" which does nothing
+        # (we're not trying to test the result of the command here)
+        return ["true"]
+
+    @property
+    def output_filename(self) -> Path:
+        return self._filename.with_suffix(".qux")
 
 
 def test_extension_visitor(tmp_path: Path):
@@ -73,29 +85,52 @@ def test_extension_visitor(tmp_path: Path):
 
     db = SqliteStateDatabase(tmp_path)
     file_info = FileInfoDatabase(db)
+    clear_tracker()
 
-    emap = {'.foo': DummyAnalyserFoo,
-            '.bar': DummyAnalyserBar}
+    emap: Dict[str, Union[Type[Task], Type[Command]]] = {
+        '.foo': DummyAnalyser,
+        '.bar': DummyCommand
+        }
     test_unit = ExtensionVisitor(emap, db, tmp_path)
     test_unit.visit(tmp_path / 'file.foo')
 
-    assert tracker["FOO"] == [tmp_path / 'file.foo']
+    assert tracker["analyser"] == [tmp_path / 'file.foo']
     assert file_info.get_file_info(foo_file) \
         == FileInfo(tmp_path / 'file.foo', 345244617)
-    assert tracker["BAR"] == []
+    assert tracker["command"] == []
 
     test_unit.visit(tmp_path / 'dir' / 'file.bar')
-    assert tracker["FOO"] == [tmp_path / 'file.foo']
+    assert tracker["analyser"] == [tmp_path / 'file.foo']
     assert file_info.get_file_info(foo_file) \
         == FileInfo(tmp_path / 'file.foo', 345244617)
-    assert tracker["BAR"] == [tmp_path / 'dir' / 'file.bar']
+    assert tracker["command"] == [tmp_path / 'dir' / 'file.bar']
     assert file_info.get_file_info(bar_file) \
         == FileInfo(tmp_path / 'dir' / 'file.bar', 2333477459)
 
     test_unit.visit(tmp_path / 'file.baz')
-    assert tracker["FOO"] == [tmp_path / 'file.foo']
+    assert tracker["analyser"] == [tmp_path / 'file.foo']
     assert file_info.get_file_info(foo_file) \
         == FileInfo(tmp_path / 'file.foo', 345244617)
-    assert tracker["BAR"] == [tmp_path / 'dir' / 'file.bar']
+    assert tracker["command"] == [tmp_path / 'dir' / 'file.bar']
     assert file_info.get_file_info(bar_file) \
         == FileInfo(tmp_path / 'dir' / 'file.bar', 2333477459)
+ 
+
+def test_nested_extension_visitor(tmp_path: Path):
+    db = StateDatabase(tmp_path)
+    clear_tracker()
+    emap: Dict[str, Union[Type[Task], Type[Command]]] = {
+        '.foo': DummyAnalyser,
+        '.bar': DummyCommand,
+        '.qux': DummyAnalyser
+        }
+    test_unit = ExtensionVisitor(emap, db, tmp_path)
+    test_unit.visit(tmp_path / 'file.foo')
+
+    assert tracker["analyser"] == [tmp_path / 'file.foo']
+    assert tracker["command"] == []
+
+    test_unit.visit(tmp_path / 'dir' / 'file.bar')
+    assert tracker["analyser"] == [tmp_path / 'file.foo',
+                                   tmp_path / 'dir' / 'file.qux']
+    assert tracker["command"] == [tmp_path / 'dir' / 'file.bar']
