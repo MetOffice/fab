94e04103e53100969a939216fc4fafcbb7a6a88c
Steve Wardle <steven.wardle@metoffice.gov.uk>
2020-03-24 14:25:18 +0000

    Non-working prototype of compilation logic

--- source/fab/application.py
+++ source/fab/application.py
@@ -8,11 +8,17 @@ import sys
 from typing import Dict, List, Type, Union
 
 from fab.database import SqliteStateDatabase
-from fab.language import Task, Command
+from fab.language import \
+    Task, \
+    Command, \
+    CommandTask, \
+    SingleFileCommand, \
+    Linker
 from fab.language.fortran import \
     FortranAnalyser, \
     FortranWorkingState, \
-    FortranPreProcessor
+    FortranPreProcessor, \
+    FortranCompiler
 from fab.source_tree import TreeDescent, ExtensionVisitor, FileInfoDatabase
 
 
@@ -20,6 +26,9 @@ class Fab(object):
     _extension_map: Dict[str, Union[Type[Task], Type[Command]]] = {
         '.f90': FortranAnalyser,
         '.F90': FortranPreProcessor,
+    }
+    _compiler_map: Dict[str, Type[SingleFileCommand]] = {
+        '.f90': FortranCompiler,
     }
 
     def __init__(self, workspace: Path, fpp_flags: str):
@@ -57,6 +66,60 @@ class Fab(object):
             for filename in files:
                 print('    found in: ' + str(filename))
                 print('    depends on: ' + str(fortran_db.depends_on(unit)))
+
+        # TODO: get name of top level unit here from the command line
+        unit_to_process = ["some_program"]
+
+        # Initialise linker
+        # TODO: again, the linker needs flags passing, and
+        #       an executable name
+        executable = Path(self._workspace / "fab_exec.exe")
+        link_command = Linker(self._workspace, [], executable)
+
+        processed_units = []
+
+        while unit_to_process:
+            # Pop pending items from start of the list
+            unit = unit_to_process.pop(0)
+            dependencies = fortran_db.depends_on(unit)
+
+            # First prune any dependencies that have already
+            # been compiled
+            for dependee in list(dependencies):
+                if dependee in processed_units:
+                    dependencies.remove(dependee)
+
+            # Add unhandled dependencies to end of list
+            if dependencies:
+                for dependee in dependencies:
+                    if dependee not in unit_to_process:
+                        unit_to_process.append(dependee)
+                # Re-add this unit after them for reconsideration
+                # note that we *do not* do this in the else branch
+                unit_to_process.append(unit)
+            else:
+                filenames = fortran_db.filenames_from_program_unit(unit)
+                # TODO: should this be done by the database?
+                #       preprocessing should ensure no duplicate units
+                if len(filenames) == 1:
+                    filename = filenames[0]
+                else:
+                    raise ValueError("Duplicate program unit found")
+
+                compiler_class = self._compiler_map[filename.suffix]
+                # TODO: Like the preprocessor need to pass any flags
+                #       through to this point eventually
+                compiler = CommandTask(
+                    compiler_class(filename, self._workspace, []))
+                # Add the object files to the linker
+                link_command.add_object(compiler.run()[0])
+                # And indicate that this unit has been processed
+                processed_units.append(unit)
+
+        # Hopefully by this point the list is exhausted and
+        # everything has been compiled, and the linker is primed
+        linker = CommandTask(link_command)
+        linker.run()
 
 
 class Dump(object):
