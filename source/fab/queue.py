4b7e45c5d13c5371e1a16d3e1b83e11fd4bc859e
Giorgia Line <giorgia.line@metoffice.gov.uk>
2020-03-26 16:59:51 +0000

    added queue manager framework

--- source/fab/queue.py
+++ source/fab/queue.py
@@ -7,7 +7,47 @@
 Classes and methods relating to the queue system
 '''
 
+from multiprocessing import Queue, Process
+from typing import List
+from pathlib import Path
+from fab.language import Task
+
+
+class StopTask(Task):
+    def run(self):
+        return []
+
+
+def worker(queue: Queue, rendezvous: List[Path]):
+    while True:
+        task = queue.get(block=True)
+        if isinstance(task, StopTask):
+            break
+        # TODO: Check here whether the task *can*
+        #       be run - if prerequisites are not
+        #       present (in rendezvous) then you
+        #       cannot run and should call
+        #       queue.put(task)
+        rendezvous.extend(task.run())
+
 
 class QueueManager(object):
-    def __init__(self):
-        pass
+    def __init__(self, n_workers: int):
+        self._queue: Queue = Queue()
+        self._n_workers = n_workers
+        self._rendezvous: List[Path] = []
+
+    def add_to_queue(self, task: Task):
+        self._queue.put(task)
+
+    def run(self):
+        for _ in range(self._n_workers):
+            process = Process(
+                target=worker, args=(self._queue,
+                                     self._rendezvous))
+            process.start()
+
+    def shutdown(self):
+        stop = StopTask()
+        for _ in range(self._n_workers):
+            self._queue.put(stop)
