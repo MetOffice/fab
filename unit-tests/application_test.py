cc651459dda43682e51c0766f07818a45b5700f6
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2020-03-31 11:06:26 +0100

    We don't unit test the top level components as that's for system testing.

---
+++ unit-tests/application_test.py
@@ -0,0 +1,11 @@
+##############################################################################
+# (c) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT
+# which you should have received as part of this distribution
+##############################################################################
+# We do not unit test the applications as they are far too complex for it.
+# They would require too many fixtures and mocks to test. At that point our
+# unit test would have become a system test.
+#
+# TODO: This is probably an indication that the application classes contain
+#       too much stuff.
---
+++ unit-tests/entry_test.py
@@ -0,0 +1,8 @@
+##############################################################################
+# (c) Crown copyright Met Office. All rights reserved.
+# For further details please refer to the file COPYRIGHT
+# which you should have received as part of this distribution
+##############################################################################
+# We do not unit test the application entry points as they are far too complex
+# for it. They would require too many fixtures and mocks to test. At that
+# point our unit test would have become a system test.
