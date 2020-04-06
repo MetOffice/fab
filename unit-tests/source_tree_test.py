d1b173ab6344826c16029be0a8b654eaf9b1b4a8
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2020-04-06 15:06:11 +0100

    Extended unit test coverage of source_tree.py

--- unit-tests/source_tree_test.py
+++ unit-tests/source_tree_test.py
@@ -4,148 +4,157 @@
 # which you should have received as part of this distribution
 ##############################################################################
 from pathlib import Path
-from typing import Iterator, Union, List, Mapping, Dict, Type
+import pytest
+from typing import Union, List, Dict, Type
 
-from fab.database import SqliteStateDatabase, FileInfoDatabase, FileInfo
+from fab.database import SqliteStateDatabase
 from fab.language import Analyser, Command, SingleFileCommand, Task
-from fab.reader import TextReader
 from fab.source_tree import ExtensionVisitor, TreeDescent, TreeVisitor
 from fab.queue import QueueManager
 
 
-class DummyVisitor(TreeVisitor):
-    def __init__(self):
-        self.visited = []
+class TestExtensionVisitor(object):
+    def test_analyser(self, tmp_path: Path):
+        (tmp_path / 'test.foo').write_text("First file")
+        (tmp_path / 'directory').mkdir()
+        (tmp_path / 'directory' / 'file.foo').write_text("Second file in directory")
 
-    def visit(self, candidate: Path) -> List[Path]:
-        self.visited.append(candidate)
-        return []
+        tracker: List[Path] = []
 
+        class DummyTask(Analyser):
+            def run(self):
+                tracker.append(self._reader.filename)
 
-def test_descent(tmp_path: Path):
-    tree_root = tmp_path / 'alpha'
-    tree_root.mkdir()
-    (tree_root / 'beta').mkdir()
-    (tree_root / 'gamma').write_text('File in root')
-    (tree_root / 'beta' / 'delta').write_text('File in beta')
-    (tree_root / 'beta' / 'epsilon').write_text('Another file in beta')
-
-    test_unit = TreeDescent(tree_root)
-    visitor = DummyVisitor()
-    test_unit.descend(visitor)
-
-    assert visitor.visited == [tree_root / 'gamma',
-                               tree_root / 'beta' / 'epsilon',
-                               tree_root / 'beta' / 'delta']
-
-
-class DummyReader(TextReader):
-    @property
-    def filename(self) -> Union[Path, str]:
-        return Path('/dummy')
-
-    def line_by_line(self) -> Iterator[str]:
-        yield 'dummy'
-
-
-tracker: Mapping[str, List[Path]] = {
-    'analyser': [],
-    'command': [],
-}
-
-
-def setup_function():
-    tracker['analyser'] = []
-    tracker['command'] = []
-
-
-class DummyAnalyser(Analyser):
-    def run(self):
-        tracker['analyser'].append(self._reader.filename)
-
-
-class DummyCommand(SingleFileCommand):
-    @property
-    def as_list(self) -> List[str]:
-        tracker['command'].append(self._filename)
-        return ['cp', str(self._filename), str(self.output[0])]
-
-    @property
-    def output(self) -> List[Path]:
-        return [self._filename.with_suffix('.baz')]
-
-
-def test_extension_visitor(tmp_path: Path):
-    foo_file = tmp_path / 'file.foo'
-    foo_file.write_text('First file')
-    (tmp_path / 'dir').mkdir()
-    bar_file = tmp_path / 'dir' / 'file.bar'
-    bar_file.write_text('Second file in subdirectory')
-    baz_file = tmp_path / 'file.baz'  # Doesn't exist
-
-    db = SqliteStateDatabase(tmp_path)
-    file_info = FileInfoDatabase(db)
-
-    emap: Dict[str, Union[Type[Task], Type[Command]]] = {
-        '.foo': DummyAnalyser,
-        '.bar': DummyCommand
+        db = SqliteStateDatabase(tmp_path)
+        emap: Dict[str, Union[Type[Task], Type[Command]]] = {
+            '.foo': DummyTask
         }
-    queue = QueueManager(1)
-    test_unit = ExtensionVisitor(emap, {}, db, tmp_path, queue)
-    test_unit.visit(foo_file)
+        fmap: Dict[Type[Command], List[str]] = {}
+        queue = QueueManager(1)
+        test_unit = ExtensionVisitor(emap, fmap, db, tmp_path, queue)
+        tracker.clear()
+        test_unit.visit(tmp_path / 'test.foo')
+        assert tracker == [tmp_path / 'test.foo']
 
-    assert tracker['analyser'] == [foo_file]
-    assert file_info.get_file_info(foo_file) \
-        == FileInfo(foo_file, 345244617)
-    assert tracker['command'] == []
+        test_unit.visit(tmp_path / 'directory' / 'file.foo')
+        assert tracker == [tmp_path / 'test.foo',
+                           tmp_path / 'directory' / 'file.foo']
 
-    test_unit.visit(bar_file)
-    assert tracker['analyser'] == [foo_file]
-    assert file_info.get_file_info(foo_file) \
-        == FileInfo(foo_file, 345244617)
-    assert tracker['command'] == [bar_file]
-    assert file_info.get_file_info(bar_file) \
-        == FileInfo(bar_file, 2333477459)
+    def test_command(self, tmp_path: Path):
+        (tmp_path / 'test.bar').write_text("File the first")
+        (tmp_path / 'directory').mkdir()
+        (tmp_path / 'directory' / 'test.bar').write_text("File the second in directory")
 
-    # Baz doesn't exist, so we're expecting no change
-    test_unit.visit(baz_file)
-    assert tracker['analyser'] == [foo_file]
-    assert file_info.get_file_info(foo_file) \
-        == FileInfo(foo_file, 345244617)
-    assert tracker['command'] == [bar_file]
-    assert file_info.get_file_info(bar_file) \
-        == FileInfo(bar_file, 2333477459)
+        tracker: List[Path] = []
 
+        class DummyCommand(SingleFileCommand):
+            @property
+            def output(self) -> List[Path]:
+                return [Path(tmp_path / 'wiggins')]
 
-def test_nested_visit(tmp_path: Path):
-    tree_root = tmp_path / 'nested'
-    tree_root.mkdir()
-    foo_file = tree_root / 'file.foo'
-    foo_file.write_text('First file')
-    bar_file = tree_root / 'file.bar'
-    bar_file.write_text('Second file')
-    # This file doesn't exist - it will be created by the
-    # command task when run on the "bar" file
-    baz_file = tree_root / 'file.baz'
+            @property
+            def as_list(self) -> List[str]:
+                tracker.append(self._filename)
+                return ['cp', str(self._filename), str(self.output[0])]
 
-    db = SqliteStateDatabase(tmp_path)
-    file_info = FileInfoDatabase(db)
-
-    emap: Dict[str, Union[Type[Task], Type[Command]]] = {
-        '.foo': DummyAnalyser,
-        '.bar': DummyCommand,
-        '.baz': DummyAnalyser
+        db = SqliteStateDatabase(tmp_path)
+        emap: Dict[str, Union[Type[Task], Type[Command]]] = {
+            '.bar': DummyCommand
         }
-    queue = QueueManager(1)
-    visitor = ExtensionVisitor(emap, {}, db, tmp_path, queue)
-    test_unit = TreeDescent(tree_root)
-    test_unit.descend(visitor)
+        fmap: Dict[Type[Command], List[str]] = {}
+        queue = QueueManager(1)
+        test_unit = ExtensionVisitor(emap, fmap, db, tmp_path, queue)
+        tracker.clear()
+        test_unit.visit(tmp_path / 'test.bar')
+        assert tracker == [tmp_path / 'test.bar']
 
-    assert tracker['analyser'] == [foo_file, baz_file]
-    assert file_info.get_file_info(foo_file) \
-        == FileInfo(foo_file, 345244617)
-    assert file_info.get_file_info(baz_file) \
-        == FileInfo(baz_file, 411763741)
-    assert tracker['command'] == [bar_file]
-    assert file_info.get_file_info(bar_file) \
-        == FileInfo(bar_file, 411763741)
+        test_unit.visit(tmp_path / 'directory' / 'test.bar')
+        assert tracker == [tmp_path / 'test.bar',
+                           tmp_path / 'directory/test.bar']
+
+    def test_unrecognised_extension(self, tmp_path: Path):
+        (tmp_path / 'test.what').write_text('Some test file')
+
+        tracker: List[Path] = []
+
+        class DummyAnalyser(Analyser):
+            tracker: List[Path] = []
+
+            def run(self):
+                tracker.append(self._reader.filename)
+
+        db = SqliteStateDatabase(tmp_path)
+        emap: Dict[str, Union[Type[Task], Type[Command]]] = {
+            '.expected': DummyAnalyser
+        }
+        fmap: Dict[Type[Command], List[str]] = {}
+        queue = QueueManager(1)
+        test_unit = ExtensionVisitor(emap, fmap, db, tmp_path, queue)
+        tracker.clear()
+        test_unit.visit(tmp_path / 'test.what')
+        assert tracker == []
+
+    def test_bad_extension_map(self, tmp_path: Path):
+        (tmp_path / 'test.qux').write_text('Test file')
+
+        class WhatnowCommand(Command):
+            @property
+            def as_list(self) -> List[str]:
+                return []
+
+            @property
+            def output(self) -> List[Path]:
+                return []
+
+            @property
+            def input(self) -> List[Path]:
+                return []
+
+        db = SqliteStateDatabase(tmp_path)
+        emap: Dict[str, Union[Type[Task], Type[Command]]] = {
+            '.qux': WhatnowCommand,
+            }
+        fmap: Dict[Type[Command], List[str]] = {}
+        queue = QueueManager(1)
+        test_unit = ExtensionVisitor(emap, fmap, db, tmp_path, queue)
+        with pytest.raises(TypeError):
+            test_unit.visit(tmp_path / 'test.qux')
+
+
+class TestTreeDescent(object):
+    @pytest.fixture(scope='class',
+                    params=[None, 'nest'])
+    def root_dir(self, request):
+        yield request.param
+
+    class DummyVisitor(TreeVisitor):
+        def __init__(self, outfile: Path):
+            self._outfile = outfile
+            self.visited: List[Path] = []
+
+        def visit(self, candidate: Path) -> List[Path]:
+            self.visited.append(candidate)
+            return []
+
+        @property
+        def output(self) -> List[Path]:
+            return [self._outfile]
+
+    def test_descent(self, tmp_path: Path, root_dir):
+        if root_dir is not None:
+            tree_root = tmp_path / root_dir
+            tree_root.mkdir()
+        else:
+            tree_root = tmp_path
+        (tree_root / 'alpha').write_text('File in root')
+        (tree_root / 'beta').mkdir()
+        (tree_root / 'beta' / 'gamma').write_text('File in beta')
+        (tree_root / 'beta' / 'delta').write_text('Another file in beta')
+
+        test_unit = TreeDescent(tree_root)
+        visitor = self.DummyVisitor(tmp_path / 'foo')
+        test_unit.descend(visitor)
+
+        assert visitor.visited == [tree_root / 'beta' / 'gamma',
+                                   tree_root / 'beta' / 'delta',
+                                   tree_root / 'alpha']
