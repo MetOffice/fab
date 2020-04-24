d55ab9f3ca5bc6ce735dd1ce455f2d44776a2ae7
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2020-04-24 15:58:01 +0100

    Example of the sort of nonsense which may be found in Fortran source trees.

---
+++ TestCases/FortranSourceTree/fpp_one.f90
@@ -0,0 +1,21 @@
+#if CHOOSE == ONE
+module fpp_mod
+
+    implicit none
+
+    public fpp_choice
+
+contains
+
+    function fpp_choice()
+
+        implicit none
+
+        character(3) :: fpp_choice
+
+        fpp_choice = "ONE"
+
+    end function fpp_choice
+
+end module fpp_mod
+#endif
