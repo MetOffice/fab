94748da8c14aa39449190f59cae4fbe2bacafc8a
bblay <byronblay@googlemail.com>
2021-12-22 15:16:22 +0000

    thinking about the code required to set and override config, which will later informt how the user will encode such rules

--- um_run.py
+++ um_run.py
@@ -80,6 +80,71 @@ def main():
     ### END OF DONE-BYGRAB STUFF
 
 
+    class Thing(object):
+        def __init__(self, fpp_flags, fc_flags):
+            self.fpp_flags = fpp_flags
+            self.fc_flags = fc_flags
+
+    foo = Thing(
+        fpp_flags=["-DUM_JULES"],
+        fc_flags=[],
+    )
+
+    shum = OtherThing(
+        pattern="/home/h02/bblay/git/fab/tmp-workspace/um/output/shumlib",
+        action=lambda foo: foo.fc_flags.extend(["-std=2018"])
+    )
+
+    if analysed_file.fpath.startswith(shum.pattern):
+        shum.action(foo)
+
+
+
+
+    # todo: replace -O2 with -O1
+
+    foo = Thing(
+        fpp_flags=[],
+        fc_flags=["-O2", "-foo"],
+    )
+
+    def set_fc_flag(foo, flag_name, flag_val):
+        # remove existing flag_name
+        pass
+
+        # set new
+        foo.fc_flags.append(f"-{flag_name}={flag_val}")
+
+    shum = OtherThing(
+        pattern="/home/h02/bblay/git/fab/tmp-workspace/um/output/shumlib",
+        action=lambda foo: set_fc_flag(foo, "O", 1)
+    )
+
+    if analysed_file.fpath.startswith(shum.pattern):
+        shum.action(foo)
+
+
+
+
+
+    # complete replace
+    fc_flags = ["-foo=bar"]         ->          fc_flags = ["-brain=hurts"]
+
+    # form from existing parts
+    fc_flags = thing1 + thing2
+
+    # replace single flag
+    action = set(fc_flags, "-O", "1")           -> ["-foo=bar", "-O2"]  -> ["-foo=bar", "-O1"]
+    action = set(fc_flags, "-std", "f2018")     -> ["-foo=bar", "-std=gnu"] -> ["-foo=bar", "-std=2018"]
+
+
+
+
+
+
+
+
+
 
 
 
@@ -89,20 +154,23 @@ def main():
     flags = config['flags']
 
     my_fab = Fab(
+        # fab behaviour
+        n_procs=3,
+        stop_on_error=True,
+        use_multiprocessing=False,
+        debug_skip=True,
+        # dump_source_tree=True
+
+        # build config
         workspace=workspace,
         target=settings['target'],
         exec_name=settings['exec-name'],
         fpp_flags=flags['fpp-flags'],
         fc_flags=flags['fc-flags'],
         ld_flags=flags['ld-flags'],
-        n_procs=3,
-        stop_on_error=True,
         skip_files=config.skip_files,
         unreferenced_deps=config.unreferenced_deps,
-        use_multiprocessing=False,
-        debug_skip=True,
-        include_paths=config.include_paths,
-        # dump_source_tree=True
+        include_paths=config.include_paths,  # todo: not clear if for pp or comp
      )
 
     logger = logging.getLogger('fab')
