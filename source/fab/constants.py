1504fa07a53946e9f6c82968d5f586b4b927afa3
bblay <byronblay@googlemail.com>
2022-03-21 15:26:41 +0000

    fix old merge conflict

--- source/fab/constants.py
+++ source/fab/constants.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-=======
 ##############################################################################
 # (c) Crown copyright Met Office. All rights reserved.
 # For further details please refer to the file COPYRIGHT
 # which you should have received as part of this distribution
 ##############################################################################
 
->>>>>>> d56b99f... copyright with autoformat
 # folders underneath workspace
 SOURCE_ROOT = "source"
 BUILD_OUTPUT = "build_output"
