3b6386622013b12f9dba31a43570aefb66af9872
bblay <byronblay@googlemail.com>
2022-06-15 12:26:35 +0100

    removed dupe moved code

--- run_configs/lfric/grab_lfric.py
+++ run_configs/lfric/grab_lfric.py
@@ -22,21 +22,6 @@ def gpl_utils_source_config():
     )
 
 
-def other_projects_source_config():
-    # for building atm
-    return BuildConfig(
-        project_label='lfric source',
-        steps=[
-            GrabFcm(src='fcm:um.xm_tr/src', dst_label='um', revision=109936),
-            GrabFcm(src='fcm:jules.xm_tr/src', dst_label='jules', revision=23182),
-            GrabFcm(src='fcm:socrates.xm_tr/src', dst_label='socrates', revision='um12.2'),
-            GrabFcm(src='fcm:casim.xm_tr/src', dst_label='casim', revision='um12.2'),
-            GrabFcm(src='fcm:shumlib.xm_tr/', dst_label='shumlib', revision='um12.2'),
-        ]
-    )
-
-
 if __name__ == '__main__':
     lfric_source_config().run()
     gpl_utils_source_config().run()
-    other_projects_source_config().run()
