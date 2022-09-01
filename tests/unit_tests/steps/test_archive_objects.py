f2c5ecf51aa5f4973f3f8edce7edad5fc4ca5826
byron <byronblay@gmail.com>
2022-09-01 09:48:59 +0100

    first test passes

--- tests/unit_tests/steps/test_archive_objects.py
+++ tests/unit_tests/steps/test_archive_objects.py
@@ -2,7 +2,7 @@ from unittest import mock
 from unittest.mock import call
 
 from fab.build_config import BuildConfig
-from fab.constants import COMPILED_FILES, BUILD_OUTPUT
+from fab.constants import BUILD_OUTPUT, OBJECT_FILES, OBJECT_ARCHIVES
 from fab.steps.archive_objects import ArchiveObjects
 
 
@@ -12,35 +12,40 @@ class Test_archive_objects(object):
         # as used when archiving before linking exes
         ar = ArchiveObjects()
         config = BuildConfig('proj')
-        targets = ['prog1', 'prog2']
 
-        artefact_store = {COMPILED_FILES: {target: [f'{target}.o', 'util.o'] for target in targets}}
+        targets = ['prog1', 'prog2']
+        artefact_store = {OBJECT_FILES: {target: [f'{target}.o', 'util.o'] for target in targets}}
 
         with mock.patch('fab.steps.archive_objects.run_command') as mock_run_command:
             ar.run(artefact_store=artefact_store, config=config)
 
-        # todo: replace with new property when available
-        expect_output_fpaths = []
-
-        expect_output_fpaths = [
-            str(config.project_workspace / BUILD_OUTPUT / 'prog1.a'),
-            str(config.project_workspace / BUILD_OUTPUT / 'prog2.a'),
+        # ensure the correct command line calls were made
+        expected_calls = [
+            call(['ar', 'cr', str(config.build_output / f'{target}.a'), f'{target}.o', 'util.o'])
+            for target in targets
         ]
+        mock_run_command.assert_has_calls(expected_calls)
 
-        expected_calls = [call() for prog in progs]
-
-        mock_run_command.assert_has_calls([
-            call('ar', 'cr', ),
-        ])
-
-        assert artefact_store[foo] = foo
+        # ensure the correct artefacts were created
+        assert artefact_store[OBJECT_ARCHIVES] == {
+            target: [str(config.build_output / f'{target}.a')] for target in targets}
 
     def test_for_library(self):
         # as used when building an object archive or archiving before linking a shared library
-        artefact_store = {COMPILED_FILES: {
-            None: ['util1.o', 'util2.o'],
-        }}
+        pass
 
+        config = BuildConfig('proj')
+        ar = ArchiveObjects(output_fpath=config.build_output / 'mylib.a')
+
+        artefact_store = {OBJECT_FILES: {None: ['util1.o', 'util2.o']}}
+
+        with mock.patch('fab.steps.archive_objects.run_command') as mock_run_command:
+            ar.run(artefact_store=artefact_store, config=config)
+
+        # ensure the correct command line calls were made
         mock_run_command.assert_called_once_with([
-            'ar', 'cr',
-        ])
+            'ar', 'cr', str(config.build_output / f'mylib.a'), f'util1.o', 'util2.o'])
+
+        # ensure the correct artefacts were created
+        assert artefact_store[OBJECT_ARCHIVES] == {
+            None: [str(config.build_output / 'mylib.a')]}
