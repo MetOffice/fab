12971bbc777d286c6c4c5b6b624f0984e0cbed32
bblay <byronblay@googlemail.com>
2022-10-05 14:11:39 +0100

    revert comments

--- system-tests-new/run-configs/run-configs.sh
+++ system-tests-new/run-configs/run-configs.sh
@@ -9,34 +9,34 @@ SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
 cd "$SCRIPT_DIR"/../../run_configs
 echo $(pwd)
 
-#echo
-#echo "building gcom variants"
-#echo
-#cd gcom
-#  ./grab_gcom.py
-#  ./build_gcom_ar.py
-#  ./build_gcom_so.py
-#cd ..
-#
-#echo
-#echo "building jules variants"
-#echo
-#cd jules
-#  ./build_jules.py
+echo
+echo "building gcom variants"
+echo
+cd gcom
+  ./grab_gcom.py
+  ./build_gcom_ar.py
+  ./build_gcom_so.py
+cd ..
+
+echo
+echo "building jules variants"
+echo
+cd jules
+  ./build_jules.py
 #  ./build_jules.py --two-stage
 #  ./build_jules.py -opt=O2
 #  ./build_jules.py -opt=O2 --two-stage
-#cd ..
-#
-#echo
-#echo "building um variants"
-#echo
-#cd um
-#  ./build_um.py
-##  ./build_um.py --two-stage
-##  ./build_um.py -opt=O2
-##  ./build_um.py -opt=O2 --two-stage
-#cd ..
+cd ..
+
+echo
+echo "building um variants"
+echo
+cd um
+  ./build_um.py
+#  ./build_um.py --two-stage
+#  ./build_um.py -opt=O2
+#  ./build_um.py -opt=O2 --two-stage
+cd ..
 
 echo
 echo "building lfric sub-projects variants"
