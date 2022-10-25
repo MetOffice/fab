834363d2a614e8332087ee6bfb8bac7d6fb4f3aa
bblay <byronblay@googlemail.com>
2022-10-25 10:47:19 +0100

    dev version bump 1.0.0.dev

--- source/fab/__init__.py
+++ source/fab/__init__.py
@@ -10,7 +10,7 @@ Flexible build system for scientific software.
 import logging
 import sys
 
-__version__ = '0.10.0'
+__version__ = '1.0.0.dev'
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler(sys.stdout))
