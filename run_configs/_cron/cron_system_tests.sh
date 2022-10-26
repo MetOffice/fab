7e4e8364d79f188c542e5333ff506de6459256e0
bblay <byronblay@googlemail.com>
2022-10-26 13:10:28 +0100

    timings

--- run_configs/_cron/cron_system_tests.sh
+++ run_configs/_cron/cron_system_tests.sh
@@ -1,12 +1,9 @@
 #
-# Build then rebuild all the MO projects, for both compilers.
-# Use a fresh clone of Fab.
+# Build and rebuild all our Met Office example build projects, for both compilers.
+# Use a fresh clone of Fab's master branch.
 #
 # Cron this to run every weekday midnight with
 # 0 0 * * 1-5 bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh 2>&1 >/dev/null"
-#
-# Or test at a specific time, e.g 12:55 pm
-# 55 12 * * * bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh"
 #
 
 set -e
@@ -16,17 +13,17 @@ mkdir /tmp/persistent/cron_system_tests
 cd /tmp/persistent/cron_system_tests
 
 # todo: we want to clone the main repo but it's blocked in the cron
-git clone --branch master --depth 1 https://github.com/metomi/fab.git
-#git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/ 2>&1 >/dev/null
+#git clone --branch master --depth 1 https://github.com/metomi/fab.git
+git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/ 2>&1 >/dev/null
 
 1>&2 echo "build all gfortran"
-./fab/run_configs/_cron/build_all_gfortran.sh
+time ./fab/run_configs/_cron/build_all_gfortran.sh
 1>&2 echo "rebuild all gfortran"
-./fab/run_configs/_cron/build_all_gfortran.sh
+time ./fab/run_configs/_cron/build_all_gfortran.sh
 
 1>&2 echo "build all ifort"
-./fab/run_configs/_cron/build_all_ifort.sh
+time ./fab/run_configs/_cron/build_all_ifort.sh
 1>&2 echo "rebuild all ifort"
-./fab/run_configs/_cron/build_all_ifort.sh
+time ./fab/run_configs/_cron/build_all_ifort.sh
 
 1>&2 echo "builds completed"
