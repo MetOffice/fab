d3ea9f5b7a24e4a3851740678d9ec259a090e6de
bblay <byronblay@googlemail.com>
2022-10-26 12:51:11 +0100

    remote repo test

--- run_configs/_cron/cron_system_tests.sh
+++ run_configs/_cron/cron_system_tests.sh
@@ -3,9 +3,10 @@
 # Use a fresh clone of Fab.
 #
 # Cron this to run every weekday midnight with
-# 0 0 * * 1-5
-# */5 * * * * bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh"
-# */5 * * * * bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh 2>&1 >/dev/null"
+# 0 0 * * 1-5 bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh 2>&1 >/dev/null"
+#
+# Or test at a specific time, e.g 12:55 pm
+# 55 12 * * * bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh"
 #
 
 set -e
@@ -14,14 +15,13 @@ rm -rf /tmp/persistent/cron_system_tests
 mkdir /tmp/persistent/cron_system_tests
 cd /tmp/persistent/cron_system_tests
 
-# todo: we want to clone the blessed master but it's blocked in the cron
-#git clone --branch master --depth 1 https://github.com/metomi/fab.git
-#git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/
-git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/ 2>&1 >/dev/null
+# todo: we want to clone the main repo but it's blocked in the cron
+git clone --branch master --depth 1 https://github.com/metomi/fab.git
+#git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/ 2>&1 >/dev/null
 
 1>&2 echo "build all gfortran"
 ./fab/run_configs/_cron/build_all_gfortran.sh
-1>&2 echo "re build all gfortran"
+1>&2 echo "rebuild all gfortran"
 ./fab/run_configs/_cron/build_all_gfortran.sh
 
 1>&2 echo "build all ifort"
