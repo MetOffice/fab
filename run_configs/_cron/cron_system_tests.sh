d996901d9c335e23431ac318850531ffe516d17e
bblay <byronblay@googlemail.com>
2022-10-26 10:09:26 +0100

    can't clone repo from cron

--- run_configs/_cron/cron_system_tests.sh
+++ run_configs/_cron/cron_system_tests.sh
@@ -12,8 +12,9 @@ rm -rf /tmp/persistent/cron_system_tests
 mkdir /tmp/persistent/cron_system_tests
 cd /tmp/persistent/cron_system_tests
 
+# todo: we want to clone the blessed master but it's blocked in the cron
 #git clone --branch master --depth 1 https://github.com/metomi/fab.git
-git clone --branch cron_local_tests --depth 1 https://github.com/bblay/fab.git
+git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/
 
 ./fab/run_configs/_cron/build_all_gfortran.sh
 ./fab/run_configs/_cron/build_all_gfortran.sh
