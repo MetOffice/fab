94a1fb7e1710a2a1d1f4eae509a9ac21ec6f08c3
bblay <byronblay@googlemail.com>
2022-10-26 11:36:42 +0100

    bout it

--- run_configs/_cron/cron_system_tests.sh
+++ run_configs/_cron/cron_system_tests.sh
@@ -4,7 +4,9 @@
 #
 # Cron this to run every weekday midnight with
 # 0 0 * * 1-5
-# */15 * * * *
+# */5 * * * * bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh"
+# */5 * * * * bash -l -c "~/git/fab/run_configs/_cron/cron_system_tests.sh 2>&1 >/dev/null"
+#
 
 set -e
 
@@ -14,10 +16,12 @@ cd /tmp/persistent/cron_system_tests
 
 # todo: we want to clone the blessed master but it's blocked in the cron
 #git clone --branch master --depth 1 https://github.com/metomi/fab.git
-git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/
+git clone --branch cron_local_tests --depth 1 file:///home/h02/bblay/git/fab/ 2>&1 >/dev/null
 
 ./fab/run_configs/_cron/build_all_gfortran.sh
 ./fab/run_configs/_cron/build_all_gfortran.sh
 
 ./fab/run_configs/_cron/build_all_ifort.sh
 ./fab/run_configs/_cron/build_all_ifort.sh
+
+echo "builds completed"
