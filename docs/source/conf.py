dfe6558020610f997a2ed6465c832bb596c2ba42
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2022-11-10 14:30:46 +0000

    Bump version for release.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022'
 author = 'Fab Team'
 
 # The full version, including alpha/beta/rc tags
-release = '0.10.0'
+release = '0.10.1'
 
 
 # -- General configuration ---------------------------------------------------
