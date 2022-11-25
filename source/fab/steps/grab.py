ddbac42a124a9ff17f0e94f389a4c481c6e8bce1
bblay <byronblay@googlemail.com>
2022-11-25 08:09:03 +0000

    wip

--- source/fab/steps/grab.py
+++ source/fab/steps/grab.py
@@ -9,6 +9,7 @@ Build steps for pulling source code from remote repos and local folders.
 """
 import logging
 import os
+import warnings
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Dict, Union, Optional
@@ -28,25 +29,39 @@ logger = logging.getLogger(__name__)
 
 class GrabSourceBase(Step, ABC):
     """
-    Base class for grab steps. All grab steps require a source and a folder in which to put it.
+    Base class for source grab steps.
 
     Unlike most steps, grab steps don't need to read or create artefact collections.
 
+    At runtime, when the build config is available, the destination folder is stored into `self._dst`.
+
     """
-    def __init__(self, src: str, dst: Optional[str] = None, name=None):
+    def __init__(self, src: str, dst: Optional[str] = None, revision=None, shallow: bool = True, name=None):
         """
         :param src:
-            The source location to grab. The nature of this parameter is depends on the subclass.
+            The source url to grab.
         :param dst:
-            The name of a sub folder, in the project workspace, in which to put the source.
+            The name of a sub folder in the project workspace's source folder, in which to put this source.
             If not specified, the code is copied into the root of the source folder.
+        :param revision:
+            E.g 'vn6.3' or 36615
+        :param shallow:
+            This flag causes the grab to be quick and shallow.
+            This can be faster for builds which start with an empty project folder.
+            If you project folder is persistent, it can be faster to turn this flag off,
+            allowing the source control software to only fetch changes since the last run.
         :param name:
-            Human friendly name for logger output, with sensible default.
+            Human friendly name for logger output, with a sensible default.
 
         """
-        super().__init__(name=name or f'{self.__class__.__name__} {dst or src}'.strip())
+        super().__init__(name=name or f'{self.__class__.__name__} {src} {revision}'.strip())
         self.src: str = src
         self.dst_label: str = dst or ''
+        self.revision = revision
+        self.shallow = shallow
+
+        # runtime
+        self._dst: Optional[Path] = None
 
     @abstractmethod
     def run(self, artefact_store: Dict, config):
@@ -64,6 +79,109 @@ class GrabSourceBase(Step, ABC):
         super().run(artefact_store, config)
         if not config.source_root.exists():
             config.source_root.mkdir(parents=True, exist_ok=True)
+
+        self._dst = config.source_root / self.dst_label
+
+
+# todo: checkout operation might be quicker for some use cases, add an option for this?
+class GrabFcm(GrabSourceBase):
+    """
+    Grab an FCM repo folder to the project workspace.
+
+    Example:
+
+        GrabFcm(src='fcm:jules.xm_tr/src', revision=revision, dst='src')
+
+    """
+    def run(self, artefact_store: Dict, config):
+        super().run(artefact_store, config)
+
+        src = f'{self.src}@{self.revision}' if self.revision else self.src
+        run_command(['fcm', 'export', '--force', src, str(self._dst)])
+
+
+class GrabSvn(GrabSourceBase):
+    """
+    Grab an SVN repo folder to the project workspace.
+
+    You can include a branch in the URL, for example:
+
+        GrabSvn(
+            src='https://code.metoffice.gov.uk/svn/lfric/GPL-utilities/trunk',
+            revision=36615, dst='gpl_utils')
+
+    """
+    def run(self, artefact_store: Dict, config):
+        super().run(artefact_store, config)
+
+        if not svn:
+            raise ImportError('svn not installed, unable to continue')
+
+        r = remote.RemoteClient(self.src)
+        r.export(str(self._dst), revision=self.revision, force=True)
+
+
+class GrabGit(GrabSourceBase):
+    """
+    Grab a Git repo into the project workspace.
+
+    If no revision is specified we'll get the default branch *if there is one*.
+
+    .. note::
+
+        Some git servers do not have a default branch.
+
+        Omitting the revision is **potentially unsafe** when rebuilding with a persistent workspace
+        because Fab will have to discover the current branch and update it.
+        It's possible for someone to change the branch between builds.
+
+
+    * It is considered unsafe to omit the revision because the checked out branch could change outside Fab.
+    * If no revision is specified, there *may* be a default branch which will be grabbed, e.g for GithHub repos.
+    * If a revision is specified, it should be a branch or tag.
+      * If `shallow` is not set the revision can also be a commit hash.
+    * If `shallow` is set (the default), only the given branch or tag is fetched, with no history.
+      Otherwise, the full repo is cloned, which may be slower.
+
+    Example:
+
+        GrabGit(src='https://github.com/bblay/tiny_fortran.git', revision='v0.1b')
+
+    """
+    def __init__(self, src: str, dst: Optional[str] = None, revision=None, shallow: bool = True, name=None):
+        """
+        Params as for
+        """
+        super().__init__(src=src, dst=dst, revision=revision, shallow=shallow, name=name)
+        if not self.revision:
+            raise ValueError("GrabGit (currently) requires a revision to be specifed.")
+
+    # Developers' note:
+    # GitHub doesn't have uploadpack.allowReachableSHA1InWant set so we can't clone or fetch a commit.
+
+    # hence the restriction on the revision not being a commit, with the command we use for a shallow grab.
+    # When shallow is not set, we use different commands which can accept a commit.
+    # Without this restriction there might not be much benefit to non-shallow checkouts at all.
+
+    def run(self, artefact_store: Dict, config):
+        super().run(artefact_store, config)
+
+        if self.shallow:
+            if not self._dst.exists():
+                run_command(['git', 'clone', '--branch', self.revision, '--depth', '1', self.src, str(self._dst)])
+            else:
+                run_command(['git', 'fetch', 'origin', self.revision, '--depth', '1'], cwd=str(self._dst))
+                run_command(['git', 'checkout', 'FETCH_HEAD'], cwd=str(self._dst))
+        else:
+            if not self._dst.exists():
+                run_command(['git', 'clone', self.src, str(self._dst)])
+            else:
+                run_command(['git', 'fetch', 'origin'], cwd=str(self._dst))
+
+            run_command(['git', 'checkout', self.revision], cwd=str(self._dst))
+
+
+# grabbing folders
 
 
 class GrabFolder(GrabSourceBase):
@@ -88,77 +206,8 @@ class GrabFolder(GrabSourceBase):
     def run(self, artefact_store: Dict, config):
         super().run(artefact_store, config)
 
-        dst: Path = config.source_root / self.dst_label
-        dst.mkdir(parents=True, exist_ok=True)
-
-        call_rsync(src=self.src, dst=dst)
-
-
-# todo: checkout operation might be quicker for some use cases, add an option for this?
-class GrabFcm(GrabSourceBase):
-    """
-    Grab an FCM repo folder to the project workspace.
-
-    """
-    def __init__(self, src: str, dst: Optional[str] = None, revision=None, name=None):
-        """
-        :param src:
-            Such as `fcm:jules.xm_tr/src`.
-        :param dst:
-            The name of a sub folder, in the project workspace, in which to put the source.
-            If not specified, the code is copied into the root of the source folder.
-        :param revision:
-            E.g 'vn6.3'
-        :param name:
-            Human friendly name for logger output, with sensible default.
-
-        Example:
-
-            GrabFcm(src='fcm:jules.xm_tr/src', revision=revision, dst='src')
-
-        """
-        super().__init__(src, dst, name=name or f'{self.__class__.__name__} {dst} {revision}'.strip())
-        self.revision = revision
-
-    def run(self, artefact_store: Dict, config):
-        super().run(artefact_store, config)
-
-        src = f'{self.src}@{self.revision}' if self.revision else self.src
-        run_command(['fcm', 'export', '--force', src, str(config.source_root / self.dst_label)])
-
-
-if svn:
-    class GrabSvn(GrabSourceBase):
-        """
-        Grab an SVN repo folder to the project workspace.
-
-        """
-        def __init__(self, src, dst=None, revision=None, name=None):
-            """
-            :param src:
-                Repo url.
-            :param dst:
-                The name of a sub folder, in the project workspace, in which to put the source.
-                If not specified, the code is copied into the root of the source folder.
-            :param revision:
-                E.g 36615
-            :param name:
-                Human friendly name for logger output, with sensible default.
-
-            Example:
-
-                GrabSvn(src='https://code.metoffice.gov.uk/svn/lfric/GPL-utilities/trunk',
-                           revision=36615, dst='gpl_utils')
-
-            """
-            super().__init__(src, dst, name)
-            self.revision = revision
-
-        def run(self, artefact_store: Dict, config):
-            super().run(artefact_store, config)
-
-            r = remote.RemoteClient(self.src)
-            r.export(str(config.source_root / self.dst_label), revision=self.revision, force=True)
+        self._dst.mkdir(parents=True, exist_ok=True)
+        call_rsync(src=self.src, dst=self._dst)
 
 
 class GrabPreBuild(Step):
---
+++ system-tests-new/GrabGit/test_GrabGit.py
@@ -0,0 +1,24 @@
+# ##############################################################################
+#  (c) Crown copyright Met Office. All rights reserved.
+#  For further details please refer to the file COPYRIGHT
+#  which you should have received as part of this distribution
+# ##############################################################################
+
+# rebuild scenarios:
+# no revision
+    # should just be a default branch
+# branch
+    # should identify branch and update it
+# tag
+    # what "branch" is reported?
+    # We need a tag which it not a branch
+# commit
+    # # what "branch" is reported?
+    # nothing should happen
+    # we need a commit which is not a branch or tag.
+
+
+
+
+
+# We need a tag which it not a branch
