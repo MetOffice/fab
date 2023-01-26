0036e815d0b335699908e204744bb8366c77393f
bblay <byronblay@googlemail.com>
2023-01-26 08:15:35 +0000

    wip

---
+++ system-tests-new/fcm/test_fcm.py
@@ -0,0 +1,32 @@
+# ##############################################################################
+#  (c) Crown copyright Met Office. All rights reserved.
+#  For further details please refer to the file COPYRIGHT
+#  which you should have received as part of this distribution
+# ##############################################################################
+
+class TestFcmExport(object):
+
+    def test_vanilla(self):
+        pass
+
+
+class TestFcmCheckout(object):
+
+    def test_new_folder(self):
+        pass
+
+    def test_working_copy(self):
+        pass
+
+    def test_not_working_copy(self):
+        pass
+
+    def test_clean(self):
+        pass
+
+
+class TestFcmMerge(object):
+
+    def test_working_copy(self):
+        pass
+
