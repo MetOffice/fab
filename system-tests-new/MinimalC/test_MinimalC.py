353a72c5d821f60e66618a2bc386be143cbaefcf
bblay <byronblay@googlemail.com>
2023-01-30 12:14:04 +0000

    merge & conflicts

--- system-tests-new/MinimalC/test_MinimalC.py
+++ system-tests-new/MinimalC/test_MinimalC.py
@@ -6,22 +6,21 @@
 import subprocess
 from pathlib import Path
 
+from fab.steps.grab import GrabFolder
+
 from fab.build_config import BuildConfig
 from fab.constants import EXECUTABLES
 from fab.steps.analyse import Analyse
 from fab.steps.c_pragma_injector import CPragmaInjector
 from fab.steps.compile_c import CompileC
-from fab.steps.compile_fortran import CompileFortran
-from fab.steps.grab import GrabFolder
-from fab.steps.link import LinkExe
-from fab.steps.preprocess import fortran_preprocessor, c_preprocessor
 from fab.steps.find_source_files import FindSourceFiles
-
+from fab.steps.link import LinkExe
+from fab.steps.preprocess import c_preprocessor
 
 PROJECT_SOURCE = Path(__file__).parent / 'project-source'
 
 
-def test_CFortranInterop(tmp_path):
+def test_MinimalC(tmp_path):
 
     # build
     config = BuildConfig(
@@ -30,23 +29,15 @@ def test_CFortranInterop(tmp_path):
         multiprocessing=False,
 
         steps=[
-            GrabFolder(src=PROJECT_SOURCE),
+            GrabFolder(PROJECT_SOURCE),
             FindSourceFiles(),
 
             CPragmaInjector(),
             c_preprocessor(),
-            fortran_preprocessor(preprocessor='cpp -traditional-cpp -P'),
-
             Analyse(root_symbol='main'),
-
             CompileC(compiler='gcc', common_flags=['-c', '-std=c99']),
-            CompileFortran(compiler='gfortran', common_flags=['-c']),
-            LinkExe(linker='gcc', flags=['-lgfortran']),
-            # todo: on an ubuntu vm, we needed these before the object files - investigate further
-            # [
-            #     '/lib/x86_64-linux-gnu/libc.so.6',
-            #     '/lib/x86_64-linux-gnu/libgfortran.so.5',
-            # ]
+
+            LinkExe(linker='gcc'),
         ],
     )
     config.run()
@@ -56,4 +47,4 @@ def test_CFortranInterop(tmp_path):
     command = [str(config._artefact_store[EXECUTABLES][0])]
     res = subprocess.run(command, capture_output=True)
     output = res.stdout.decode()
-    assert output == ''.join(open(PROJECT_SOURCE / 'expected.exec.txt').readlines())
+    assert output == 'Hello world!'
