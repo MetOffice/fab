39bf63e0b16789349b9b67322e883dc584eb7033
bblay <byronblay@googlemail.com>
2023-02-02 09:04:10 +0000

    review actions

--- system-tests-new/svn_fcm/test_svn_fcm.py
+++ system-tests-new/svn_fcm/test_svn_fcm.py
@@ -14,6 +14,7 @@ import warnings
 
 import pytest
 
+import fab
 from fab.steps.grab.fcm import FcmCheckout, FcmExport, FcmMerge
 from fab.steps.grab.svn import GrabSvnBase, SvnCheckout, SvnExport, SvnMerge
 
@@ -28,6 +29,11 @@ if not export_classes:
 
 
 @pytest.fixture
+def config(tmp_path):
+    return mock.Mock(source_root=tmp_path / 'fab_proj/source')
+
+
+@pytest.fixture
 def repo_url(tmp_path):
     shutil.unpack_archive(
         Path(__file__).parent / 'repo.tar.gz',
@@ -36,148 +42,176 @@ def repo_url(tmp_path):
 
 
 @pytest.fixture
-def config(tmp_path):
-    return mock.Mock(source_root=tmp_path / 'fab_proj/source')
+def trunk(repo_url):
+    # URL of the main branch.
+    return f'{repo_url}/proj/main/trunk'
+
+
+@pytest.fixture
+def file1_experiment_a(repo_url):
+    # A branch which modifies file 1.
+    return f'{repo_url}/proj/main/branches/dev/person_aK/file1_experiment_a'
+
+
+@pytest.fixture
+def file1_experiment_b(repo_url):
+    # Another branch which modifies file 1. It should conflict with experiment a.
+    return f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_b'
+
+
+@pytest.fixture
+def file2_experiment(repo_url):
+    # A branch which modifies file 2.
+    # It has two revisions, with different versions of the modification in r7 and r8.
+    return f'{repo_url}/proj/main/branches/dev/person_b/file2_experiment'
+
+
+def confirm_trunk(config) -> bool:
+    file1_txt = (config.source_root / 'proj/file1.txt').read_text()
+    file2_txt = (config.source_root / 'proj/file2.txt').read_text()
+    return file1_txt.startswith("This is sentence one in file one.") and \
+           file2_txt.strip().endswith("This is sentence two in file two.")
+
+
+def confirm_file1_experiment_a(config) -> bool:
+    # Have we got the revision 7 text in file 2?
+    file1_txt = (config.source_root / 'proj/file2.txt').read_text()
+    return file1_txt.startswith("This is sentence one, with Experiment A modification.")
+
+
+def confirm_file2_experiment_r7(config) -> bool:
+    # Have we got the revision 7 text in file 2?
+    file2_txt = (config.source_root / 'proj/file2.txt').read_text()
+    return file2_txt.strip().endswith("This is sentence two, with experimental modification.")
+
+
+def confirm_file2_experiment_r8(config) -> bool:
+    # Have we got the revision 7 text in file 2?
+    file2_txt = (config.source_root / 'proj/file2.txt').read_text()
+    return file2_txt.strip().endswith("This is sentence two, with further experimental modification.")
 
 
 class TestFcmExport(object):
 
     # Run the test twice, once with SvnExport and once with FcmExport - depending on which tools are available.
     @pytest.mark.parametrize('export_class', export_classes)
-    def test_export(self, repo_url, config, export_class):
-        export = export_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        export.run(artefact_store=None, config=config)
+    def test_export(self, file2_experiment, config, export_class):
+        # Export the "file 2 experiment" branch, which has different sentence from trunk in r1 and r2
+        export = export_class(src=file2_experiment, dst='proj', revision=7)
+        export.run(artefact_store={}, config=config)
+        assert confirm_file2_experiment_r7(config)
 
-        # Make sure we can export twice.
-        # Todo: should the export step wipe the destination first?
-        export.run(artefact_store=None, config=config)
+        # Make sure we can export twice into the same folder.
+        # Todo: should the export step wipe the destination first? To remove residual, orphaned files?
+        export = export_class(src=file2_experiment, dst='proj', revision=8)
+        export.run(artefact_store={}, config=config)
+        assert confirm_file2_experiment_r8(config)
 
 
 class TestFcmCheckout(object):
 
     @pytest.mark.parametrize('checkout_class', checkout_classes)
-    def test_new_folder(self, repo_url, config, checkout_class):
-        checkout = checkout_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        checkout.run(artefact_store=None, config=config)
-
-        file2_txt = open(config.source_root / 'proj/file2.txt').read()
-        assert "This is sentence two in file two." in file2_txt
+    def test_new_folder(self, trunk, config, checkout_class):
+        checkout = checkout_class(src=trunk, dst='proj')
+        checkout.run(artefact_store={}, config=config)
+        assert confirm_trunk(config)
 
     @pytest.mark.parametrize('checkout_class', checkout_classes)
-    def test_working_copy(self, repo_url, config, checkout_class):
-
-        # clean checkout of the "file 2 experiment" branch, which has different sentence from trunk in r1 and r2
-        checkout = checkout_class(
-            src=f'{repo_url}/proj/main/branches/dev/person_b/file2_experiment', dst='proj', revision='7')
-        checkout.run(artefact_store=None, config=config)
-
-        # check we've got the revision 7 text
-        file2_txt = open(config.source_root / 'proj/file2.txt').read()
-        assert "This is sentence two, with experimental modification." in file2_txt
-
-        # expect an update the second time, get a newer revision and todo: check the contents
-        checkout = checkout_class(
-            src=f'{repo_url}/proj/main/branches/dev/person_b/file2_experiment', dst='proj', revision='8')
-        checkout.run(artefact_store=None, config=config)
-
-        # check we've got the revision 8 text
-        file2_txt = open(config.source_root / 'proj/file2.txt').read()
-        assert "This is sentence two, with further experimental modification." in file2_txt
-
-    @pytest.mark.parametrize('export_class,checkout_class', zip(export_classes, checkout_classes))
-    def test_not_working_copy(self, repo_url, config, export_class, checkout_class):
-        # the export command just makes files, not an svn working copy
-        export = export_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        export.run(artefact_store=None, config=config)
-
-        # if we try to checkout into that folder, it should fail
-        export = checkout_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        with pytest.raises(ValueError):
-            export.run(artefact_store=None, config=config)
-
-    @pytest.mark.parametrize('checkout_class', checkout_classes)
-    def test_update(self, repo_url, config, checkout_class):
+    def test_working_copy(self, file2_experiment, config, checkout_class):
+        # Make sure we can checkout into a working copy.
         # The scenario we're testing here is checking out across multiple builds.
         # This will usually be the same revision. The first run in a new folder will be a checkout,
         # and subsequent runs will use update, which can handle a version bump.
-        f1xa = checkout_class(src=f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_a', dst='proj')
-        f1xa.run(artefact_store=None, config=config)
+        # Since we can change the revision and expect it to work, let's test that while we're here.
 
-        f1xa = checkout_class(src=f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_a', dst='proj')
-        f1xa.run(artefact_store=None, config=config)
+        with mock.patch('fab.steps.grab.svn.run_command', wraps=fab.steps.grab.svn.run_command) as wrap:
 
-    @pytest.mark.parametrize('checkout_class', checkout_classes)
-    def test_update_revision(self, repo_url, config, checkout_class):
-        # Following on from test_update(), since we CAN change the revision and expect it to work, let's test that.
-        f1xa = checkout_class(
-            src=f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_a', dst='proj', revision=7)
-        f1xa.run(artefact_store=None, config=config)
+            checkout = checkout_class(src=file2_experiment, dst='proj', revision='7')
+            checkout.run(artefact_store={}, config=config)
+            assert confirm_file2_experiment_r7(config)
+            wrap.assert_called_with([
+                checkout_class.command, 'checkout', '--revision', '7',
+                file2_experiment, str(config.source_root / 'proj')])
 
-        f1xa = checkout_class(
-            src=f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_a', dst='proj', revision=8)
-        f1xa.run(artefact_store=None, config=config)
+            checkout = checkout_class(src=file2_experiment, dst='proj', revision='8')
+            checkout.run(artefact_store={}, config=config)
+            assert confirm_file2_experiment_r8(config)
+            wrap.assert_called_with(
+                [checkout_class.command, 'update', '--revision', '8'],
+                cwd=config.source_root / 'proj')
+
+    @pytest.mark.parametrize('export_class,checkout_class', zip(export_classes, checkout_classes))
+    def test_not_working_copy(self, trunk, config, export_class, checkout_class):
+        # the export command just makes files, not a working copy
+        export = export_class(src=trunk, dst='proj')
+        export.run(artefact_store={}, config=config)
+
+        # if we try to checkout into that folder, it should fail
+        export = checkout_class(src=trunk, dst='proj')
+        with pytest.raises(ValueError):
+            export.run(artefact_store={}, config=config)
 
 
 class TestFcmMerge(object):
 
     @pytest.mark.parametrize('checkout_class,merge_class', zip(checkout_classes, merge_classes))
-    def test_vanilla(self, repo_url, config, checkout_class, merge_class):
+    def test_vanilla(self, trunk, file2_experiment, config, checkout_class, merge_class):
         # something to merge into; checkout trunk
-        checkout = checkout_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        checkout.run(artefact_store=None, config=config)
+        checkout = checkout_class(src=trunk, dst='proj')
+        checkout.run(artefact_store={}, config=config)
+        confirm_trunk(config)
 
         # merge another branch in
-        merge = merge_class(src=f'{repo_url}/proj/main/branches/dev/person_b/file2_experiment', dst='proj')
-        merge.run(artefact_store=None, config=config)
-
-        # check we've got the revision 7 text from the other branch
-        file2_txt = open(config.source_root / 'proj/file2.txt').read()
-        assert "This is sentence two, with further experimental modification." in file2_txt
+        merge = merge_class(src=file2_experiment, dst='proj')
+        merge.run(artefact_store={}, config=config)
+        confirm_file2_experiment_r8(config)
 
     @pytest.mark.parametrize('checkout_class,merge_class', zip(checkout_classes, merge_classes))
-    def test_revision(self, repo_url, config, checkout_class, merge_class):
+    def test_revision(self, trunk, file2_experiment, config, checkout_class, merge_class):
         # something to merge into; checkout trunk
-        checkout = checkout_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        checkout.run(artefact_store=None, config=config)
+        checkout = checkout_class(src=trunk, dst='proj')
+        checkout.run(artefact_store={}, config=config)
+        confirm_trunk(config)
 
         # merge another branch in
-        merge = merge_class(src=f'{repo_url}/proj/main/branches/dev/person_b/file2_experiment', dst='proj', revision=7)
-        merge.run(artefact_store=None, config=config)
-
-        # check we've got the revision 7 text from the other branch
-        file2_txt = open(config.source_root / 'proj/file2.txt').read()
-        assert "This is sentence two, with experimental modification." in file2_txt
+        merge = merge_class(src=file2_experiment, dst='proj', revision=7)
+        merge.run(artefact_store={}, config=config)
+        confirm_file2_experiment_r7(config)
 
     @pytest.mark.parametrize('export_class,merge_class', zip(export_classes, merge_classes))
-    def test_not_working_copy(self, repo_url, config, export_class, merge_class):
-        export = export_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        export.run(artefact_store=None, config=config)
+    def test_not_working_copy(self, trunk, file2_experiment, config, export_class, merge_class):
+        export = export_class(src=trunk, dst='proj')
+        export.run(artefact_store={}, config=config)
 
         # try to merge into an export
-        merge = merge_class(src=f'{repo_url}/proj/main/branches/dev/person_b/file2_experiment', dst='proj', revision=7)
+        merge = merge_class(src=file2_experiment, dst='proj', revision=7)
         with pytest.raises(ValueError):
-            merge.run(artefact_store=None, config=config)
+            merge.run(artefact_store={}, config=config)
 
     @pytest.mark.parametrize('checkout_class,merge_class', zip(checkout_classes, merge_classes))
-    def test_conflict(self, repo_url, config, checkout_class, merge_class):
-        checkout = checkout_class(src=f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_a', dst='proj')
-        checkout.run(artefact_store=None, config=config)
+    def test_conflict(self, file1_experiment_a, file1_experiment_b, config, checkout_class, merge_class):
+        checkout = checkout_class(src=file1_experiment_a, dst='proj')
+        checkout.run(artefact_store={}, config=config)
+        confirm_file1_experiment_a(config)
 
         # this branch modifies the same line of text
-        merge = merge_class(src=f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_b', dst='proj')
+        merge = merge_class(src=file1_experiment_b, dst='proj')
         with pytest.raises(RuntimeError):
-            merge.run(artefact_store=None, config=config)
+            merge.run(artefact_store={}, config=config)
 
     @pytest.mark.parametrize('checkout_class,merge_class', zip(checkout_classes, merge_classes))
-    def test_multiple_merges(self, repo_url, config, checkout_class, merge_class):
-        trunk = checkout_class(src=f'{repo_url}/proj/main/trunk', dst='proj')
-        trunk.run(artefact_store=None, config=config)
+    def test_multiple_merges(self, trunk, file1_experiment_a, file2_experiment, config, checkout_class, merge_class):
+        trunk = checkout_class(src=trunk, dst='proj')
+        trunk.run(artefact_store={}, config=config)
+        confirm_trunk(config)
 
-        f1xa = merge_class(src=f'{repo_url}/proj/main/branches/dev/person_a/file1_experiment_a', dst='proj')
-        f1xa.run(artefact_store=None, config=config)
+        f1xa = merge_class(src=file1_experiment_a, dst='proj')
+        f1xa.run(artefact_store={}, config=config)
+        confirm_file1_experiment_a(config)
 
-        fx2 = merge_class(src=f'{repo_url}/proj/main/branches/dev/person_b/file2_experiment', dst='proj', revision=7)
-        fx2.run(artefact_store=None, config=config)
+        fx2 = merge_class(src=file2_experiment, dst='proj', revision=7)
+        fx2.run(artefact_store={}, config=config)
+        confirm_file2_experiment_r7(config)
 
 
 class TestBase(object):
