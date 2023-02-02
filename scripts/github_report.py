8caf0020140afbc914960fec485a6565d9915761
bblay <byronblay@googlemail.com>
2023-02-02 10:54:01 +0000

    flake8

---
+++ scripts/github_report.py
@@ -0,0 +1,20 @@
+import json
+
+from fab.tools import run_command
+
+data_json = run_command(['gh', 'pr', 'status', '--json', 'number,author,reviews'], capture_output=True)
+data = json.loads(data_json)
+
+for pr in data['createdBy']:
+
+    print('-------------')
+    print('number', pr['number'])
+    print(f"author {pr['author']['name']} ({pr['author']['login']})")
+
+    reviewers_str = ', '.join({review['author']['login'] for review in pr['reviews']})
+    print('reviewed by', reviewers_str)
+
+    print('')
+
+# todo: there will probably be some data in here to print, not seen that yet.
+print(f"{len(data['needsReview'])} need review")
