81d951934ba619f42ace42c19670d7f3703e0bd4
dbrown <wilbertcs@gmail.com>
2023-03-17 19:23:44 +0000

    fix for tiny fortran ...maybe :)

--- run_configs/tiny_fortran/build_tiny_fortran.py
+++ run_configs/tiny_fortran/build_tiny_fortran.py
@@ -8,6 +8,7 @@ from fab.build_config import BuildConfig
 from fab.steps.analyse import analyse
 from fab.steps.compile_fortran import compile_fortran
 from fab.steps.find_source_files import find_source_files
+from fab.steps.grab.git import git_checkout
 from fab.steps.link import link_exe
 from fab.steps.preprocess import preprocess_fortran
 
@@ -15,13 +16,13 @@ from fab.steps.preprocess import preprocess_fortran
 if __name__ == '__main__':
 
     with BuildConfig(project_label=f'tiny_fortran $compiler') as config:
-        grab_git(src='https://github.com/metomi/fab-test-data.git', revision='main', dst='src'),
+        git_checkout(config, src='https://github.com/metomi/fab-test-data.git', revision='main', dst_label='src'),
 
-        find_source_files(),
+        find_source_files(config),
 
-        preprocess_fortran(preprocessor='fpp -P'),
+        preprocess_fortran(config, preprocessor='fpp -P'),
 
-        analyse(root_symbol='my_prog'),
+        analyse(config, root_symbol='my_prog'),
 
-        compile_fortran(),
-        link_exe(linker='mpifort'),
+        compile_fortran(config),
+        link_exe(config, linker='mpifort'),
