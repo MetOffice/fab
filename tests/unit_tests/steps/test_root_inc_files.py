0580c24ec545fa2a2f8b1c85c1c0739cac90e4f9
bblay <byronblay@googlemail.com>
2023-03-21 17:29:43 +0000

    all unit tests pass

--- tests/unit_tests/steps/test_root_inc_files.py
+++ tests/unit_tests/steps/test_root_inc_files.py
@@ -3,50 +3,46 @@ from unittest import mock
 
 import pytest
 
+from fab.build_config import BuildConfig
 from fab.constants import BUILD_OUTPUT
-from fab.steps.root_inc_files import RootIncFiles
-
-
-@pytest.fixture
-def common_data():
-    source_root = Path('/foo/source')
-    build_output = source_root.parent / BUILD_OUTPUT
-    return source_root, build_output
+from fab.steps.root_inc_files import root_inc_files
 
 
 class TestRootIncFiles(object):
 
-    def test_vanilla(self, common_data):
+    def test_vanilla(self):
         # ensure it copies the inc file
-        source_root, build_output = common_data
         inc_files = [Path('/foo/source/bar.inc')]
-        step = RootIncFiles()
+
+        config = BuildConfig('proj')
+        config._artefact_store['all_source'] = inc_files
 
         with mock.patch('fab.steps.root_inc_files.shutil') as mock_shutil:
             with mock.patch('fab.steps.root_inc_files.Path.mkdir'):
-                step.run(artefact_store={'all_source': inc_files}, config=mock.Mock(source_root=source_root))
+                root_inc_files(config)
 
-        mock_shutil.copy.assert_called_once_with(inc_files[0], source_root.parent / BUILD_OUTPUT)
+        mock_shutil.copy.assert_called_once_with(inc_files[0], config.build_output)
 
-    def test_skip_output_folder(self, common_data):
+    def test_skip_output_folder(self):
         # ensure it doesn't try to copy a file in the build output
-        source_root, build_output = common_data
-        inc_files = [Path('/foo/source/bar.inc'), build_output / 'fab.inc']
-        step = RootIncFiles()
+        config = BuildConfig('proj')
+        inc_files = [Path('/foo/source/bar.inc'), config.build_output / 'fab.inc']
+        config._artefact_store['all_source'] = inc_files
 
         with mock.patch('fab.steps.root_inc_files.shutil') as mock_shutil:
             with mock.patch('fab.steps.root_inc_files.Path.mkdir'):
-                step.run(artefact_store={'all_source': inc_files}, config=mock.Mock(source_root=source_root))
+                root_inc_files(config)
 
-        mock_shutil.copy.assert_called_once_with(inc_files[0], build_output)
+        mock_shutil.copy.assert_called_once_with(inc_files[0], config.build_output)
 
-    def test_name_clash(self, common_data):
+    def test_name_clash(self):
         # ensure raises an exception if there is a name clash
-        source_root, build_output = common_data
         inc_files = [Path('/foo/source/bar.inc'), Path('/foo/sauce/bar.inc')]
-        step = RootIncFiles()
+
+        config = BuildConfig('proj')
+        config._artefact_store['all_source'] = inc_files
 
         with pytest.raises(FileExistsError):
             with mock.patch('fab.steps.root_inc_files.shutil'):
                 with mock.patch('fab.steps.root_inc_files.Path.mkdir'):
-                    step.run(artefact_store={'all_source': inc_files}, config=mock.Mock(source_root=source_root))
+                    root_inc_files(config)
