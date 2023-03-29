8a4f0fd53271c4d3398be852215560e9fd9f01e0
bblay <byronblay@googlemail.com>
2023-03-29 14:55:50 +0100

    unit test

---
+++ tests/unit_tests/steps/test_preprocess.py
@@ -0,0 +1,39 @@
+# ##############################################################################
+#  (c) Crown copyright Met Office. All rights reserved.
+#  For further details please refer to the file COPYRIGHT
+#  which you should have received as part of this distribution
+# ##############################################################################
+from pathlib import Path
+from unittest import mock
+
+from fab.build_config import BuildConfig
+from fab.steps.preprocess import preprocess_fortran
+
+
+class Test_preprocess_fortran(object):
+
+    def test_vanilla(self, tmp_path):
+        # ensure big F90s are preprocessed and little f90s are copied
+
+        config = BuildConfig('proj', fab_workspace=tmp_path)
+        big_f90 = Path(config.source_root / 'big.F90')
+        little_f90 = Path(config.source_root / 'little.f90')
+
+        def source_getter(artefact_store):
+            return [big_f90, little_f90]
+
+        with mock.patch('fab.steps.preprocess.pre_processor') as mock_pp:
+            with mock.patch('shutil.copyfile') as mock_copy:
+                with config:
+                    preprocess_fortran(config=config, source=source_getter)
+
+        mock_pp.assert_called_once_with(
+            config,
+            preprocessor=mock.ANY,
+            common_flags=mock.ANY,
+            files=[big_f90],
+            output_collection=mock.ANY,
+            output_suffix='.f90',
+        )
+
+        mock_copy.assert_called_once_with(str(little_f90), mock.ANY)
