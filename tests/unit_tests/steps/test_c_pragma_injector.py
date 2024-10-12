444dd3bc85756580623c9db4a60aea6b1e37856b
Matthew Hambley <matthew.hambley@metoffice.gov.uk>
2024-10-12 11:39:07 +0100

    Inherit slightly more extensive testing of C pragma injector.

--- tests/unit_tests/steps/test_c_pragma_injector.py
+++ tests/unit_tests/steps/test_c_pragma_injector.py
@@ -1,4 +1,5 @@
-import sys
+from sys import version_info as python_version
+from textwrap import dedent
 from unittest import mock
 from unittest.mock import mock_open
 
@@ -9,32 +10,52 @@ from fab.steps.c_pragma_injector import inject_pragmas
 
 class Test_inject_pragmas(object):
 
-    @pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python3.8 or higher for mock_open iteration")
+    @pytest.mark.skipif(python_version < (3, 8),
+                        reason="Requires python version 3.8 or higher for "
+                               "mock_open iteration")
     def test_vanilla(self):
-        input = [
-            '',
-            '// hi there, ignore me',
-            '',
-            '#include <foo>',
-            '',
-            '#include "bar.h"',
-            '',
-        ]
-        data = "\n".join(input)
+        source = dedent(
+            """
+            // C++ style comment, ignore this.
+            #include "user_include.h"
+            #include "second_user_include.h"
+            Unrelated text
+            /* Including C style comment */
+            #include 'another_user_include.h'
+            #include <system_include.h>
+            More unrelated text
+            #include <another_system_include.h>
+            #include "final_user_include.h"
+            """
+        )
 
-        with mock.patch('fab.steps.c_pragma_injector.open', mock_open(read_data=data)):
+        with mock.patch('fab.steps.c_pragma_injector.open',
+                        mock_open(read_data=source)):
             result = inject_pragmas(fpath="foo")
             output = list(result)
 
         assert output == [
             '\n',
-            '// hi there, ignore me\n',
-            '\n',
-            '#pragma FAB SysIncludeStart\n',
-            '#include <foo>\n',
-            '#pragma FAB SysIncludeEnd\n',
-            '\n',
+            '// C++ style comment, ignore this.\n',
             '#pragma FAB UsrIncludeStart\n',
-            '#include "bar.h"\n',
+            '#include "user_include.h"\n',
             '#pragma FAB UsrIncludeEnd\n',
+            '#pragma FAB UsrIncludeStart\n',
+            '#include "second_user_include.h"\n',
+            '#pragma FAB UsrIncludeEnd\n',
+            'Unrelated text\n',
+            '/* Including C style comment */\n',
+            '#pragma FAB UsrIncludeStart\n',
+            "#include 'another_user_include.h'\n",
+            '#pragma FAB UsrIncludeEnd\n',
+            '#pragma FAB SysIncludeStart\n',
+            '#include <system_include.h>\n',
+            '#pragma FAB SysIncludeEnd\n',
+            "More unrelated text\n",
+            '#pragma FAB SysIncludeStart\n',
+            '#include <another_system_include.h>\n',
+            '#pragma FAB SysIncludeEnd\n',
+            '#pragma FAB UsrIncludeStart\n',
+            '#include "final_user_include.h"\n',
+            '#pragma FAB UsrIncludeEnd\n'
         ]
