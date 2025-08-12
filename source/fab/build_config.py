7bc5c0ab413e133ce9f0305ca65f2de12bcbf413
Joerg Henrichs <henrichsjoerg@gmail.com>
2025-08-12 10:50:36 +1000

    #465 Support usage of ~ when setting the fab workspace.

--- source/fab/build_config.py
+++ source/fab/build_config.py
@@ -110,8 +110,9 @@ class BuildConfig():
 
         # Make sure fab_workspace is an absolute path, some tools
         # (esp. compiler) change working directory and so need
-        # absolute paths.
-        fab_workspace = fab_workspace.resolve()
+        # absolute paths. Also support '~', in case that a user
+        # uses this when setting the fab workspace.
+        fab_workspace = fab_workspace.expanduser().resolve()
         logger.info(f"fab workspace is {fab_workspace}")
 
         self._project_workspace: Path = fab_workspace / self.project_label
