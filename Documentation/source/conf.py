b84dc413f30eae0f2b37a344f0fee70ca6804501
Yaswant Pradhan <yaswant.pradhan@metoffice.gov.uk>
2025-08-22 14:25:59 +0100

    Update LaTeX package installation and documentation generation steps

--- Documentation/source/conf.py
+++ Documentation/source/conf.py
@@ -116,6 +116,3 @@ linkcheck_ignore = [
 ]
 linkcheck_timeout = 2
 linkcheck_allow_unauthorized = True  # Allow unauthorized links to pass
-
-# pdf engine for latex builder
-latex_engine = 'pdflatex'
