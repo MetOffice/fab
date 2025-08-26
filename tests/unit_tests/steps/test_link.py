20fb00ef9003cf1919906a4901a147f2eebbd7b4
Joerg Henrichs <henrichsjoerg@gmail.com>
2025-08-26 16:07:31 +1000

    #373 Removed useless class wrapper (which triggers pylint warnings).

--- tests/unit_tests/steps/test_link.py
+++ tests/unit_tests/steps/test_link.py
@@ -24,39 +24,36 @@ from fab.tools.tool_repository import ToolRepository
 from tests.conftest import call_list
 
 
-class TestLinkExe:
+def test_run(fake_process: FakeProcess,
+             stub_fortran_compiler) -> None:
     """
-    Tests linking an executable.
+    Tests linking an executable. Make sure the command
+    is formed correctly.
     """
-    def test_run(self, fake_process: FakeProcess,
-                 stub_fortran_compiler) -> None:
-        """
-        Tests correct formation of command.
-        """
 
-        version_command = ['sfc', '--version']
-        fake_process.register(version_command, stdout='1.2.3')
-        link_command = ['sfc', 'bar.o', 'foo.o',
-                        '-L/my/lib', '-lmylib', '-fooflag', '-barflag',
-                        '-o', '/fab/link_test/foo']
-        fake_process.register(link_command, stdout='abc\ndef')
+    version_command = ['sfc', '--version']
+    fake_process.register(version_command, stdout='1.2.3')
+    link_command = ['sfc', 'bar.o', 'foo.o',
+                    '-L/my/lib', '-lmylib', '-fooflag', '-barflag',
+                    '-o', '/fab/link_test/foo']
+    fake_process.register(link_command, stdout='abc\ndef')
 
-        linker = Linker(compiler=stub_fortran_compiler)
-        linker.add_lib_flags('mylib', ['-L/my/lib', '-lmylib'])
+    linker = Linker(compiler=stub_fortran_compiler)
+    linker.add_lib_flags('mylib', ['-L/my/lib', '-lmylib'])
 
-        tool_box = ToolBox()
-        tool_box.add_tool(stub_fortran_compiler)
-        tool_box.add_tool(linker)
+    tool_box = ToolBox()
+    tool_box.add_tool(stub_fortran_compiler)
+    tool_box.add_tool(linker)
 
-        config = BuildConfig('link_test', tool_box, fab_workspace=Path('/fab'),
-                             mpi=False, openmp=False, multiprocessing=False)
-        config.artefact_store[ArtefactSet.OBJECT_FILES] = \
-            {'foo': {'foo.o', 'bar.o'}}
+    config = BuildConfig('link_test', tool_box, fab_workspace=Path('/fab'),
+                         mpi=False, openmp=False, multiprocessing=False)
+    config.artefact_store[ArtefactSet.OBJECT_FILES] = \
+        {'foo': {'foo.o', 'bar.o'}}
 
-        with warns(UserWarning,
-                   match="_metric_send_conn not set, cannot send metrics"):
-            link_exe(config, libs=['mylib'], flags=['-fooflag', '-barflag'])
-        assert call_list(fake_process) == [version_command, link_command]
+    with warns(UserWarning,
+               match="_metric_send_conn not set, cannot send metrics"):
+        link_exe(config, libs=['mylib'], flags=['-fooflag', '-barflag'])
+    assert call_list(fake_process) == [version_command, link_command]
 
 
 def test_run_select_linker_fortran(fake_process: FakeProcess,
