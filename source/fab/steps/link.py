be8b8374d0e58756fe25d48842822d8429857494
Joerg Henrichs <henrichsjoerg@gmail.com>
2025-09-04 18:09:43 +1000

    #379 Fixed duplicated code.

--- source/fab/steps/link.py
+++ source/fab/steps/link.py
@@ -92,9 +92,7 @@ def link_exe(config,
 
     libs = libs or []
     flags = flags or []
-    source_getter = source or DefaultLinkerSource()
 
-    target_objects = source_getter(config.artefact_store)
     for root, objects in target_objects.items():
         exe_path = config.project_workspace / f'{root}'
         linker.link(objects, exe_path, config=config, libs=libs,
